9cc0de9b66ca5ae4ce915708ed49d239bf92fea8
buquio <kehinde.bukola14@gmail.com>
2021-04-07 11:58:10 +0100

    initial commit

---
+++ 10es6-import-export-and-modules-completed/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+// OR  not good
+import React from "react";
+import ReactDOM from "react-dom";
+import * as pi from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
